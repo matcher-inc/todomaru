019cd9e8abe77cafa61bf821982413b87e6fdb93
渡部真伍 <s1170087@gmail.com>
2018-05-11 19:10:20 +0900

    change: JSON(lowerUpperCase) & Rails(snake_case)

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Force JSON key name to lower camelCase
+Jbuilder.key_format(camelize: :lower)
+
+ActionDispatch::Request.parameter_parsers[:json] = -> (raw_post) do
+  # Modified from action_dispatch/http/parameters.rb
+  data = ActiveSupport::JSON.decode(raw_post)
+  data = {:_json => data} unless data.is_a?(Hash)
+
+  # Transform camelCase param keys to snake_case:
+  data.deep_transform_keys! do |key|
+    key[0].match(/^[[:upper:]]/) ? key : key.underscore
+  end
+end
