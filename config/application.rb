7f200d23dcfad2797b9f0091cf4590537d8f19ea
Watanabe Shingo <s1170087@gmail.com>
2018-05-12 16:58:06 +0000

    change: to disable automatic generation of assets and helper in rails generate command

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module Todomaru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Disable assets & helper auto generation in rails generate command
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
