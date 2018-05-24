c0be57acc9b9cd0e438b316590b9a0d4f93e9237
teshiharu <vagrant@localhost.localdomain>
2018-05-24 10:09:10 +0000

    add: todo createform のガワだけつくった

--- app/javascript/packs/web/components/todos/CreateForm.tsx
+++ app/javascript/packs/web/components/todos/CreateForm.tsx
@@ -4,7 +4,23 @@ export default class CreateForm extends React.Component<any, any> {
   render() {
     return (
       <div className={this.props.className + ' todos-CreateForm'}>
-        createForm
+        <form class="todos-CreateForm_form">
+          <div class="todos-CreateForm_formRow">
+            <label class="todos-CreateForm_label">タイトル</label>
+            <input type="text" class="todos-CreateForm_textField"></input>
+          </div>
+          <div class="todos-CreateForm_formRow">
+            <label class="todos-CreateForm_label">期日</label>
+            <input type="text" class="todos-CreateForm_textField"></input>
+          </div>
+          <div class="todos-CreateForm_formRow">
+            <label class="todos-CreateForm_label">詳細</label>
+            <textarea class="todos-CreateForm_textArea" />
+          </div>
+          <div class="todos-CreateForm_formRow">
+            <input type="submit" class="todos-CreateForm_submit"></input>
+          </div>
+        </form>
       </div>
     );
   }
