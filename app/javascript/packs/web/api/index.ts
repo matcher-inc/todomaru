749fdb8479a6a3f9909968e2c46e3be588b9a976
teshiharu <vagrant@localhost.localdomain>
2018-05-30 09:29:54 +0000

    add: todosをゲットして表示、postして新規作成

--- app/javascript/packs/web/api/index.ts
+++ app/javascript/packs/web/api/index.ts
@@ -8,3 +8,10 @@ export const axios = _axios.create({
   xsrfHeaderName: 'X-CSRF-Token',
   baseURL: `${location.protocol}//${location.host}`,
 });
+
+const todosUrl = '/users/me/todos';
+export const todos = {
+  //index: params => { return axios.post(todosUrl, params) },
+  index: () => { return axios.get('https://api.myjson.com/bins/17jy56') },
+  create: (params) => { return axios.post(todosUrl, params) },
+}
--- app/javascript/packs/web/pages/todos/Index.tsx
+++ app/javascript/packs/web/pages/todos/Index.tsx
@@ -1,42 +1,37 @@
 import * as React from 'react';
+import * as api from 'web/api';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { CreateForm, Todo, Header } from 'web/components/todos';
-
-// const todosUrl = '/todos'
-const todosUrl = 'https://api.myjson.com/bins/17jy56'
 
 export class Index extends React.Component<any, any> {
   constructor(props) {
     super(props);
     this.state = { todos: [] };
-
     this.loadTodos();
+
     this.addTodo = this.addTodo.bind(this)
     this.createTodo = this.createTodo.bind(this)
     this.onTodoClick = this.onTodoClick.bind(this)
   }
 
   loadTodos() {
-    axios
-      .get(todosUrl)
+    api
+      .todos
+      .index()
       .then(res => this.setState({ todos: res.data }));
   }
 
   addTodo(todo) {
-    todo.id = 123;
     this.state.todos.push(todo);
     this.setState({ todos: this.state.todos });
   }
 
-  createTodo(params) {
-    axios
-      .post(todosUrl, {todo: params})
-      .then(res => this.addTodo(params));
-
-    // TODO: ちゃんとレスポンスデータを入れる
-    let tmp = JSON.parse(JSON.stringify(params));
-    this.addTodo(tmp)
+  createTodo(todo) {
+    api
+      .todos
+      .create({todo})
+      .then(res => this.addTodo(res.data));
   }
 
   onTodoClick(todo) {
