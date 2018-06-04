0a9316391ca9f097e512cc861c87182a95df49f9
Watanabe Shingo <s1170087@gmail.com>
2018-06-04 09:56:07 +0000

    add: common files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     def set_csrf_cookie
       cookies['X-CSRF-Token'] = form_authenticity_token if protect_against_forgery?
     end
+
+    def set_user_id_cookie
+      cookies['user_id'] = current_user.id if sign_in?
+    end
 end
